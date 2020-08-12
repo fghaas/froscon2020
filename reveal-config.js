33fb6345ececc2142ccdfcb2d111e759a6532b0a
Florian Haas <xahteiwi@gmail.com>
2020-08-12 15:19:26 +0200

    Disable multiplex

--- reveal-config.js
+++ reveal-config.js
@@ -21,12 +21,6 @@ Reveal.initialize({
         markers: true
     },
 
-    multiplex: {
-        secret: multiplex_config['secret'],
-        id: multiplex_config['socketId'],
-        url: "https://reveal-js-multiplex-ccjbegmaii.now.sh"
-    },
-
     // Optional libraries used to extend on reveal.js
     dependencies: [
         { src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
@@ -35,10 +29,6 @@ Reveal.initialize({
         { src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
         { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
         { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
-
-        { src: '//cdn.socket.io/socket.io-1.3.5.js', async: true },
-        { src: 'reveal.js/plugin/multiplex/client.js', async: true },
-
 
         { src: 'reveal.js-menu/menu.js', async: true, condition: function() { return !!document.body.classList; } },
 
